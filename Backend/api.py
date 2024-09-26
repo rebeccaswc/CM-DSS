468ed97c3e3809e9c63c515273b0e228f72f50d3
rebeccaswc <rebecca200381@gmail.com>
2024-09-26 20:05:40 +0800

    refactor: adjust Backend port

--- Backend/api.py
+++ Backend/api.py
@@ -248,4 +248,5 @@ def refresh():
     return jsonify(ret), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
