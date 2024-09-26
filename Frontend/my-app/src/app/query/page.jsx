3ed280e26b22639178affe6b50d447c8f9cc7b4d
rebeccaswc <rebecca200381@gmail.com>
2024-09-27 01:35:16 +0800

    feat: add Suspense to /query/page.jsx

--- Frontend/my-app/src/app/query/page.jsx
+++ Frontend/my-app/src/app/query/page.jsx
@@ -1,7 +1,6 @@
-
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react"; // 從 React 中導入 Suspense
 import Header from "./Header";
 import Summary from "./Summary";
 import Sidebar from "./Sidebar";
@@ -9,7 +8,6 @@ import ChatInput from "../../components/ui/ChatInput";
 import { useSearchParams } from 'next/navigation';
 
 function Chatpage() {
-
   const searchParams = useSearchParams();
   const alertID = searchParams.get('alertID');
 
@@ -19,12 +17,10 @@ function Chatpage() {
         <Sidebar />
       </div>
       <div className="w-10/12 flex flex-col">
-        <Header
-        alertID={alertID} />
+        <Header alertID={alertID} />
         <section className="flex-1 flex flex-col bg-[#1d203e] px-20 py-11 overflow-y-auto max-md:px-5">
           <div className="flex-1 w-full max-w-[765px] mx-auto overflow-y-scroll">
-            <Summary
-            index={alertID} />
+            <Summary index={alertID} />
           </div>
           <div className="w-full max-w-[765px] mx-auto mt-4">
             <ChatInput />
@@ -35,4 +31,12 @@ function Chatpage() {
   );
 }
 
-export default Chatpage;
+const Page = () => {
+  return (
+    <Suspense fallback={<div>載入中...</div>}>
+      <Chatpage />
+    </Suspense>
+  );
+};
+
+export default Page;
