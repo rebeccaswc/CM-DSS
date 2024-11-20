8558b201d58ed0dfda3014aaaf063a0b2787bfa2
rebeccaswc <rebecca200381@gmail.com>
2024-11-20 23:04:04 +0800

    feat: add jwt token in middleware

--- Frontend/my-app/src/middleware.ts
+++ Frontend/my-app/src/middleware.ts
@@ -4,11 +4,11 @@ import type { NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("access_token_cookie")?.value;
 
-  // // 驗證 token 是否存在
-  // if (!token) {
-  //   console.log("No token found, redirecting to Login Page");
-  //   return NextResponse.redirect(new URL("/login", req.url));
-  // }
+  // 驗證 token 是否存在
+  if (!token) {
+    console.log("No token found, redirecting to Login Page");
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 
   return NextResponse.next();
 }
