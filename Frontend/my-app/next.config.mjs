569111278f44f9bba092e5a53a8b7b368f8d11ca
rebeccaswc <rebecca200381@gmail.com>
2024-11-24 23:09:35 +0800

    feat: adjust backend api

--- Frontend/my-app/next.config.mjs
+++ Frontend/my-app/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+        async rewrites() {
+          return [
+            {
+              source: '/api/:path*', // 捕捉所有以 /api/ 開頭的路徑
+              destination: 'https://cm-dss.onrender.com/api/:path*', // 轉發到後端 URL
+            },
+          ];
+        },
+      };
+      
+      export default nextConfig;
--- Frontend/my-app/src/app/api/summary.js
+++ Frontend/my-app/src/app/api/summary.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const apiUrl = process.env.NEXT_PUBLIC_BACKEND_API;
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export const getSummary = async (index) => {
     try {
