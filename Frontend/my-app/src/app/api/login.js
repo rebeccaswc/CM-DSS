90d627e4f06604c9c4108e5d9973f392b4923fec
rebeccaswc <rebecca200381@gmail.com>
2024-12-02 13:09:21 +0800

    feat: add jwt expiry date

--- Frontend/my-app/src/app/api/login.js
+++ Frontend/my-app/src/app/api/login.js
@@ -1,5 +1,15 @@
 import api from './api';
 
+function setAccessToken(token) {
+  const now = new Date();
+  const expiry = now.getTime() + 30 * 24 * 60 * 60 * 1000; // 30天後
+  const data = {
+    token,
+    expiry,
+  };
+
+  localStorage.setItem('access_token', JSON.stringify(data));
+}
 
 export const logInWithPythonService = async (email, password) => {
   try {
@@ -9,10 +19,7 @@ export const logInWithPythonService = async (email, password) => {
     });
 
     if (response && response.data) {
-      // document.cookie = `access_token=${response.data.access_token}; Path=/; Secure; SameSite=Strict`;
-      localStorage.setItem('access_token', response.data.token);
-      console.log(response.data.token)
-      return response.data;
+      setAccessToken(response.data.access_token);
     }
   } catch (e) {
     console.error("Invalid credentials", e);
--- Frontend/my-app/src/components/ui/AuthGuard.jsx
+++ Frontend/my-app/src/components/ui/AuthGuard.jsx
@@ -7,9 +7,19 @@ const AuthGuard = ({ children }) => {
   const router = useRouter();
 
   useEffect(() => {
-    const token = localStorage.getItem('access_token');
+    const accessTokenData = localStorage.getItem('access_token');
 
-    if (!token) {
+    if (!accessTokenData) {
+      router.push('/login');
+      return;
+    }
+
+    const { token, expiry } = JSON.parse(accessTokenData);
+    const now = new Date().getTime();
+
+    if (!token || now > expiry) {
+      // 如果 token 不存在或已過期，清除 localStorage 並跳轉到 login 頁面
+      localStorage.removeItem('access_token');
       router.push('/login');
     }
   }, [router]);
